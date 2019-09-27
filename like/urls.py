c753227576172061dfe1f45dbf3c13ca85850dbc
bravekjel <53958814+bravekjel@users.noreply.github.com>
2019-09-27 15:20:01 +0900

    Delete urls.py

--- like/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('like/list/', views.api_like_list),
-    path('like/', views.api_like),
-    path('unlike/', views.api_unlike),
-]
